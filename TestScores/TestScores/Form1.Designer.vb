cff2b4e9649e0d528e878262053f5cdafa1d276c
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-11-29 10:34:16 -0600

    update

---
+++ TestScores/TestScores/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGetScores = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAverages = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGetScores
+        '
+        Me.btnGetScores.Location = New System.Drawing.Point(33, 156)
+        Me.btnGetScores.Name = "btnGetScores"
+        Me.btnGetScores.Size = New System.Drawing.Size(75, 23)
+        Me.btnGetScores.TabIndex = 0
+        Me.btnGetScores.Text = "Get Scores"
+        Me.btnGetScores.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(156, 156)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Test Averages"
+        '
+        'lblAverages
+        '
+        Me.lblAverages.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverages.Location = New System.Drawing.Point(143, 60)
+        Me.lblAverages.Name = "lblAverages"
+        Me.lblAverages.Size = New System.Drawing.Size(100, 23)
+        Me.lblAverages.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblAverages)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGetScores)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGetScores As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblAverages As System.Windows.Forms.Label
+
+End Class
---
+++ TestScores/TestScores/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+    Private Sub btnGetScores_Click(sender As Object, e As EventArgs) Handles btnGetScores.Click
+        Dim intNumScores As Integer
+        Dim dblTestScore As Double
+        Dim dblTotal As Double = 0
+        Dim dblAverage As Double
+        Dim strInput As String
+        Dim intCount As Integer = 1
+
+        strInput = InputBox("How many test scores do you want to average")
+        If Integer.TryParse(strInput, intNumScores) Then
+            Do Until intCount > intNumScores
+                strInput = InputBox("Enter test score" & intCount.ToString())
+                If Double.TryParse(strInput, dblTestScore) Then
+                    dblTotal += dblTestScore
+                    intCount += 1
+                Else
+                    MessageBox.Show("Enter a numeric test score.")
+                End If
+
+            Loop
+            If intNumScores > 0 Then
+                dblAverage = dblTotal / intNumScores
+                lblAverages.Text = dblAverage.ToString()
+            End If
+        Else
+            MessageBox.Show("Entrer an integer value for number of test scorea.")
+
+        End If
+    End Sub
+End Class
